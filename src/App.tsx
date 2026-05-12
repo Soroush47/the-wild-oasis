72f5eb5cf8cfda0ca371a23553bb6176e3849ff6
Soroush Ghasemi <soroush.341379@gmail.com>
2026-05-12 12:46:06 +0330

    Style Component added to the project for styling and be typed

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,44 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Heading from "./ui/Heading";
+import Input from "./ui/Input";
+import Button from "./ui/Button";
+import Row from "./ui/Row";
+
+const StyledApp = styled.main`
+    /* background-color: red; */
+    padding: 20px;
+`;
+
 function App() {
-    return <div>Hello World</div>;
+    return (
+        <>
+            <GlobalStyles />
+            <StyledApp>
+                <Row type="horizontal">
+                    <Heading>The Wild Oasis</Heading>
+                    <div>
+                        <Heading as="h2">Check in and out</Heading>
+                        <Button onClick={() => alert("Check in")}>Check in</Button>
+                        <Button
+                            variation="danger"
+                            size="small"
+                            onClick={() => alert("Check out")}
+                        >
+                            Check out
+                        </Button>
+                    </div>
+                </Row>
+                <Row type="vertical">
+                    <Heading as="h3">Form</Heading>
+                    <form>
+                        <Input type="number" placeholder="Number of guests" />
+                        <Input type="number" placeholder="Number of guests" />
+                    </form>
+                </Row>
+            </StyledApp>
+        </>
+    );
 }
 
 export default App;
