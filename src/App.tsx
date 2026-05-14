1e66ad55e22cbfd89111ccafeda3bc1e1571e815
Soroush Ghasemi <soroush.341379@gmail.com>
2026-05-14 10:21:11 +0330

    Some type errors have been checked, display of AppLayout changed to grid, Sidebar and Header component added to the project, react router added to the project.

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,39 @@
-import styled from "styled-components";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Bookings from "./pages/Bookings";
+import Cabins from "./pages/Cabins";
+import Users from "./pages/Users";
+import Settings from "./pages/Settings";
+import Account from "./pages/Account";
+import Login from "./pages/Login";
+import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./ui/AppLayout";
 import GlobalStyles from "./styles/GlobalStyles";
-import Heading from "./ui/Heading";
-import Input from "./ui/Input";
-import Button from "./ui/Button";
-import Row from "./ui/Row";
-
-const StyledApp = styled.main`
-    /* background-color: red; */
-    padding: 20px;
-`;
 
 function App() {
     return (
         <>
             <GlobalStyles />
-            <StyledApp>
-                <Row type="horizontal">
-                    <Heading>The Wild Oasis</Heading>
-                    <div>
-                        <Heading as="h2">Check in and out</Heading>
-                        <Button onClick={() => alert("Check in")}>Check in</Button>
-                        <Button
-                            variation="danger"
-                            size="small"
-                            onClick={() => alert("Check out")}
-                        >
-                            Check out
-                        </Button>
-                    </div>
-                </Row>
-                <Row type="vertical">
-                    <Heading as="h3">Form</Heading>
-                    <form>
-                        <Input type="number" placeholder="Number of guests" />
-                        <Input type="number" placeholder="Number of guests" />
-                    </form>
-                </Row>
-            </StyledApp>
+            <BrowserRouter
+            // future={{
+            //     v7_startTransition: true,
+            //     v7_relativeSplatPath: true,
+            // }}
+            >
+                <Routes>
+                    <Route element={<AppLayout />}>
+                        <Route index element={<Navigate to="dashboard" replace />} />
+                        <Route path="dashboard" element={<Dashboard />} />
+                        <Route path="bookings" element={<Bookings />} />
+                        <Route path="cabins" element={<Cabins />} />
+                        <Route path="users" element={<Users />} />
+                        <Route path="settings" element={<Settings />} />
+                        <Route path="account" element={<Account />} />
+                    </Route>
+                    <Route path="login" element={<Login />} />
+                    <Route path="*" element={<PageNotFound />} />
+                </Routes>
+            </BrowserRouter>
         </>
     );
 }
