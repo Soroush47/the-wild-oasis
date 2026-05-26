4b89acc31e9d37861e67059b797a149e53338ea1
Soroush Ghasemi <soroush.341379@gmail.com>
2026-05-26 20:45:49 +0330

    Create Table Compound Component

--- src/features/cabins/CabinRow.tsx
+++ src/features/cabins/CabinRow.tsx
@@ -8,17 +8,18 @@ import { useCreateCabin } from "./useCreateCabin";
 import CreateCabinForm from "./CreateCabinForm";
 import Modal from "../../ui/Modal";
 import ConfirmDelete from "../../ui/ConfirmDelete";
+import Table from "../../ui/Table";
 
-const TableRow = styled.div`
-    display: grid;
-    grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
-    column-gap: 2.4rem;
-    align-items: center;
-    padding: 1.4rem 2.4rem;
-    &:not(:last-child) {
-        border-bottom: 1px solid var(--color-grey-100);
-    }
-`;
+// const TableRow = styled.div`
+//     display: grid;
+//     grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
+//     column-gap: 2.4rem;
+//     align-items: center;
+//     padding: 1.4rem 2.4rem;
+//     &:not(:last-child) {
+//         border-bottom: 1px solid var(--color-grey-100);
+//     }
+// `;
 
 const Img = styled.img`
     display: block;
@@ -86,47 +87,45 @@ function CabinRow({ cabin }: CabinRowProps) {
     };
 
     return (
-        <>
-            <TableRow role="row">
-                <Img src={image} />
-                <Cabin>{name}</Cabin>
-                <div>Fits up to {maxCapacity} guests</div>
-                <Price>{formatCurrency(regularPrice)}</Price>
-                {discount ? (
-                    <Discount>{formatCurrency(discount)}</Discount>
-                ) : (
-                    <span>&mdash;</span>
-                )}
-                <div>
-                    <button onClick={handleDuplicate} disabled={isCreating}>
-                        <HiSquare2Stack />
-                    </button>
-                    <Modal>
-                        <Modal.Open opens="edit-cabin">
-                            <button
-                            //  size="small"
-                            >
-                                <HiPencil />
-                            </button>
-                        </Modal.Open>
-                        <Modal.Window name="edit-cabin">
-                            <CreateCabinForm cabin={cabin} />
-                        </Modal.Window>
-                        <Modal.Open opens="delete-cabin">
-                            <button
-                            // $variation="danger" //Transient Props
-                            // size="small"
-                            >
-                                <HiTrash />
-                            </button>
-                        </Modal.Open>
-                        <Modal.Window name="delete-cabin">
-                            <ConfirmDelete resourceName="Cabins" cabinId={cabinId} />
-                        </Modal.Window>
-                    </Modal>
-                </div>
-            </TableRow>
-        </>
+        <Table.Row>
+            <Img src={image} />
+            <Cabin>{name}</Cabin>
+            <div>Fits up to {maxCapacity} guests</div>
+            <Price>{formatCurrency(regularPrice)}</Price>
+            {discount ? (
+                <Discount>{formatCurrency(discount)}</Discount>
+            ) : (
+                <span>&mdash;</span>
+            )}
+            <div>
+                <button onClick={handleDuplicate} disabled={isCreating}>
+                    <HiSquare2Stack />
+                </button>
+                <Modal>
+                    <Modal.Open opens="edit-cabin">
+                        <button
+                        //  size="small"
+                        >
+                            <HiPencil />
+                        </button>
+                    </Modal.Open>
+                    <Modal.Window name="edit-cabin">
+                        <CreateCabinForm cabin={cabin} />
+                    </Modal.Window>
+                    <Modal.Open opens="delete-cabin">
+                        <button
+                        // $variation="danger" //Transient Props
+                        // size="small"
+                        >
+                            <HiTrash />
+                        </button>
+                    </Modal.Open>
+                    <Modal.Window name="delete-cabin">
+                        <ConfirmDelete resourceName="Cabins" cabinId={cabinId} />
+                    </Modal.Window>
+                </Modal>
+            </div>
+        </Table.Row>
     );
 }
 
