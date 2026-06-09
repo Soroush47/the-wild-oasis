269022cac518dd54a7672229eab191fe41858bd6
Soroush Ghasemi <soroush.341379@gmail.com>
2026-06-09 10:12:53 +0330

    Use Prefetching for booking pagination

--- src/features/bookings/useBookings.ts
+++ src/features/bookings/useBookings.ts
@@ -1,6 +1,7 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings";
 import { useSearchParams } from "react-router-dom";
+import { PAGE_SIZE } from "../../utils/constants";
 
 type FilterValue = "unconfirmed" | "checked-in" | "checked-out" | "all";
 
@@ -17,6 +18,7 @@ type SortBy =
     | "createdAt-asc";
 
 export function useBookings() {
+    const queryClient = useQueryClient();
     const [searchParams] = useSearchParams();
 
     const filterValue = (searchParams.get("status") || "all") as FilterValue;
@@ -30,6 +32,20 @@ export function useBookings() {
         queryKey: ["bookings", filter, sortBy, page],
         queryFn: () => getBookings(filter, sortBy, page),
     });
+
+    const pageCount = Math.ceil(data?.data.count / PAGE_SIZE);
+
+    if (page < pageCount)
+        queryClient.prefetchQuery({
+            queryKey: ["bookings", filter, sortBy, page + 1],
+            queryFn: () => getBookings(filter, sortBy, page + 1),
+        });
+
+    if (page > 1)
+        queryClient.prefetchQuery({
+            queryKey: ["bookings", filter, sortBy, page - 1],
+            queryFn: () => getBookings(filter, sortBy, page - 1),
+        });
 
     return { data, isLoading };
 }
