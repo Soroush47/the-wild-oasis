f0c964feb3a1b5e0c946580521cc8f73ccceb5cf
Soroush Ghasemi <soroush.341379@gmail.com>
2026-07-03 20:08:42 +0330

    Add backend url

--- src/configs/api.ts
+++ src/configs/api.ts
@@ -1,10 +1,9 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:3001/api";
+const BASE_URL = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
-    baseURL: BASE_URL, //express
-    // baseURL: "http://localhost:5000",  //json-server
+    baseURL: BASE_URL,
     headers: { "Content-Type": "application/json" },
 });
 
